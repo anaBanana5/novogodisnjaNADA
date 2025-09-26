09843dabd48baf0a32e94977cbd82743ac718a29
anaBanana5 <ana.topic3@skole.hr>
2025-09-26 16:10:04 +0200

    promijenjena boja botuna

--- novogodisnjaNADA/Form2.Designer.cs
+++ novogodisnjaNADA/Form2.Designer.cs
@@ -208,12 +208,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Location = new System.Drawing.Point(381, 285);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(159, 66);
             this.button1.TabIndex = 18;
             this.button1.Text = "SPREMI";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
